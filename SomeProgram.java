11678f17768392ddd3f41e5efaca762ac621219e
Dawid Kotula <dawid.kotula@interia.pl>
2026-01-11 09:50:25 +0100

    #3 Second commit was empty

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,10 @@
 public class SomeProgram {
+	
 	public static void main (String [] args {
+		
 		System.out.printls("We are learning to use Git.");
+		
+		System.out.println("We will be Git experts, soon.");
+
 	}
 }
